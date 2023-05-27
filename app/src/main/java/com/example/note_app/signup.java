3ff0db55c5c26f94ed646d4ffe1362667473c5d1
Bongjun Kim <kimbong523@gmail.com>
2023-05-27 16:30:25 +0900

    part-4_finish

---
+++ app/src/main/java/com/example/note_app/signup.java
@@ -0,0 +1,59 @@
+package com.example.note_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class signup extends AppCompatActivity {
+
+    private EditText msignupemail, msignuppassword;
+    private RelativeLayout msignup;
+    private TextView mgotologin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        getSupportActionBar().hide();
+
+        msignupemail=findViewById(R.id.signupemail);
+        msignuppassword=findViewById(R.id.signuppassword);
+        msignup=findViewById(R.id.signup);
+        mgotologin=findViewById(R.id.gotologin);
+
+        mgotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signup.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        msignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=msignupemail.getText().toString().trim();
+                String password=msignuppassword.getText().toString().trim();
+
+                if(mail.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "All Fields are Required", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length()<7) {
+                    Toast.makeText(getApplicationContext(), "Password Should Greater than 7 Digits", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //regitered the user to firebase
+                }
+            }
+        });
+    }
+}
