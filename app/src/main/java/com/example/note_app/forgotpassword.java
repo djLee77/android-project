591b58429696d9c06e057a39de8682e310d7d96a
Bongjun Kim <kimbong523@gmail.com>
2023-05-28 23:35:31 +0900

    captcha_please

--- app/src/main/java/com/example/note_app/forgotpassword.java
+++ app/src/main/java/com/example/note_app/forgotpassword.java
@@ -1,7 +1,5 @@
 package com.example.note_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +8,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class forgotpassword extends AppCompatActivity {
 
     private EditText mforgotpassword;
     private Button mpasswordrecoverbutton;
     private TextView mgobacktologin;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,28 +30,49 @@ public class forgotpassword extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        mforgotpassword=findViewById(R.id.forgotpassword);
-        mpasswordrecoverbutton=findViewById(R.id.passwordrecoverbutton);
-        mgobacktologin=findViewById(R.id.gobacktologin);
+        mforgotpassword = findViewById(R.id.forgotpassword);
+        mpasswordrecoverbutton = findViewById(R.id.passwordrecoverbutton);
+        mgobacktologin = findViewById(R.id.gobacktologin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
 
         mgobacktologin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent=new Intent(forgotpassword.this, MainActivity.class);
-                    startActivity(intent);
+                Intent intent = new Intent(forgotpassword.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
         mpasswordrecoverbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mail=mforgotpassword.getText().toString().trim();
-                if(mail.isEmpty()){
+                String mail = mforgotpassword.getText().toString().trim();
+                if (mail.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Enter your mail first", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     //we have to send password recover email
+
+                    firebaseAuth.sendPasswordResetEmail(mail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Mail Sent , You can recover your password using mail", Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(forgotpassword.this, MainActivity.class));
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Email is Wrong or Account Not Exist", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                    });
+
                 }
             }
         });
     }
+
 }
--- app/src/main/java/com/example/note_app/signup.java
+++ app/src/main/java/com/example/note_app/signup.java
@@ -1,7 +1,5 @@
 package com.example.note_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +8,21 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signup extends AppCompatActivity {
 
     private EditText msignupemail, msignuppassword;
     private RelativeLayout msignup;
     private TextView mgotologin;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,8 @@ public class signup extends AppCompatActivity {
         msignuppassword=findViewById(R.id.signuppassword);
         msignup=findViewById(R.id.signup);
         mgotologin=findViewById(R.id.gotologin);
+
+        firebaseAuth= FirebaseAuth.getInstance();
 
         mgotologin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,13 +55,43 @@ public class signup extends AppCompatActivity {
                 if(mail.isEmpty() || password.isEmpty()){
                     Toast.makeText(getApplicationContext(), "All Fields are Required", Toast.LENGTH_SHORT).show();
                 }
-                else if(password.length()<7) {
+                else if(password.length()<7){
                     Toast.makeText(getApplicationContext(), "Password Should Greater than 7 Digits", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     //regitered the user to firebase
+                    firebaseAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                                sendEmailVerification();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(), "Failed To Register", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
                 }
             }
         });
+    }
+    private void sendEmailVerification() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser!=null) {
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(), "Verification Email is Sent, Verify and Log In Again", Toast.LENGTH_SHORT).show();
+                    firebaseAuth.signOut();
+                    finish();
+                    startActivity(new Intent(signup.this, MainActivity.class));
+                }
+            });
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Failed TO Send Verification Email", Toast.LENGTH_SHORT).show();
+        }
     }
 }
